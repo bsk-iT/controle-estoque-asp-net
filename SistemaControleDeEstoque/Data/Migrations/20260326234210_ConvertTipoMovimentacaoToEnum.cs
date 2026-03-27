4d654a903e213f53e6df0f8b70800ac1bd38e3a2
bsk-iT <bruno.cubero10@gmail.com>
2026-03-26 21:14:14 -0300

    fix: W-6 migration (converter TipoMovimentacao para enum)
    - W-6: Migration que converte tipo string para enum TipoMovimentacao
      - EF Core gerou Up() vazio (esperado com HasConversion<string>)
      - Contém SQL UPDATE manual para migrar dados existentes
      - Garante dados consistentes com novo modelo

---
+++ SistemaControleDeEstoque/Data/Migrations/20260326234210_ConvertTipoMovimentacaoToEnum.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SistemaControleDeEstoque.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConvertTipoMovimentacaoToEnum : Migration
+    {
+    /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.Sql("UPDATE dbo.Movimentacao SET Tipo = 'Saida' WHERE Tipo = N'Saída'");
+        }
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          migrationBuilder.Sql("UPDATE dbo.Movimentacao SET Tipo = N'Saída' WHERE Tipo = 'Saida'");
+        }
+    }
+}
